1f30a819b95feaaa5a03c660ac5e02078e200ca0
Evgenia S <sdevgenia@gmail.com>
2018-07-17 15:44:01 +0300

    Initial Commit

---
+++ iComicBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iComicBooks
+//
+//  Created by Sviridova Evgenia on 17.07.2018.
+//  Copyright © 2018 Sviridova Evgenia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
